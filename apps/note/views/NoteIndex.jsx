39a2e50b6e75b697e881dd557c80fdf7806e1362
PescadoMuerto1 <mbl0502727793@egmail.com>
2024-03-13 12:13:29 +0200

    Add book index component

--- apps/note/views/NoteIndex.jsx
+++ apps/note/views/NoteIndex.jsx
@@ -1,4 +1,26 @@
 
+const { useState, useEffect } = React
+
+import { noteService } from '../services/note.service.js'
+
 export function NoteIndex() {
-    return <div>note app</div>
+    const [notes, setNotes] = useState([])
+
+    useEffect(() => {
+        loadNotes()
+    }, [])
+
+    function loadNotes() {
+        noteService.query()
+            // .then((notes) => sortNotes(notes, sortBy))
+            .then(setNotes)
+            .catch(console.error)
+    }
+
+    if (!notes) return <div>loading...</div>
+    return (
+        <div className='content-layout'>
+            
+        </div>
+    )
 }
