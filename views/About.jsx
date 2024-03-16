b3001b9bfd225adc6d054d8123a07545b43ccd55
PescadoMuerto1 <mbl0502727793@egmail.com>
2024-03-17 00:18:43 +0200

    Add about page

--- views/About.jsx
+++ views/About.jsx
@@ -1,6 +1,15 @@
 
 export function About() {
     return <section className="about">
+        <img src="/assets/img/sus-2.jpg" alt="" />
+        <img src="/assets/img/sus-3.jpg" alt="" />
         <h1>About Page</h1>
+
+        <p>Hello and welcome! We are <strong>yifat and moshe</strong>, the dynamic duo behind <strong>Apsus App</strong>. As passionate individuals with a shared vision, we embarked on this journey to create something truly remarkable.</p>
+        <p>Driven by our mutual interests and complementary skills, we joined forces to bring our ideas to life. We found common ground in our dedication learn and to continuously improve our craft </p>
+        <p>Our collaboration thrives on open communication, mutual respect, and a relentless pursuit of excellence. Through countless brainstorming sessions, late-night discussions, and shared moments of inspiration, we have forged a partnership built on trust and mutual support.</p>
+        <p>Our project represents the culmination of our combined efforts, reflecting not only our individual talents but also our shared commitment to innovation and creativity. From the initial concept to the final product, every step of the journey has been a testament to our determination and perseverance.</p>
+        <p>Above all, we are grateful for the opportunity to collaborate and learn from one another. Together, we aspire to make a meaningful impact and leave a lasting legacy through our work.</p>
+        <p>Thank you for joining us on this incredible adventure.</p>
     </section>
 }
